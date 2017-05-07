8271460313d0dc92f8c42d33a654ed3329aabbf8
Jull Weber <jull@monkeykodeagency.com>
2017-05-07 12:17:43 -0700

    setup webpack project and vue and add basic scss

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
+import './assets/main.scss'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  // router,
+  // template: '<App/>',
+  // components: {App}
+  render: h => h(App)
 })
+
